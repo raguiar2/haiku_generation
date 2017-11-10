ef2fb5d602aac857b24ab15e5fe65622f7c57fc4
Kevin Liao <liaok@stanford.edu>
2017-11-10 00:55:36 -0800

    First implementation of reformatting other poems

---
+++ baseline/poem_filtering.py
@@ -0,0 +1,45 @@
+# This file reads in all of the poems from the file "other_poets" 
+# and puts them into one large corpus called "all_other_poems.txt". 
+
+import os
+import random
+import collections
+import string
+POEMS_DIR = 'other_poets'
+
+def read_poem(file):
+    lines = file.readlines()
+    with open('all_other_poems.txt','a') as txtfile:
+        for line in lines:
+            fixedtext = ""
+            # delete line numbers
+            for char in line:
+                if char.isdigit():
+                    continue
+                else:
+                    fixedtext += char
+            # strip EOL. 
+            fixedtext = fixedtext.strip()
+            # write to txt file
+            txtfile.write(fixedtext.lower() + '\n')
+
+def read_poems():
+    with open('all_other_poems.txt', 'w') as txtfile:
+        txtfile.write('All other poems filtered. Used as a corpus')
+        txtfile.write('\n\n')
+	for file in os.listdir(POEMS_DIR):
+		# skip hidden files
+		if file[0] == '.':
+			continue
+		filepath = POEMS_DIR+'/'+file
+		with open(filepath) as file:
+			read_poem(file)
+
+def main():
+    print('Reading in poems...')
+    read_poems()
+    print('Poems read!')
+
+
+if __name__ == "__main__":
+	main()
