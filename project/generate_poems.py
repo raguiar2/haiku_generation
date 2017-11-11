62287e960fbda39aead96ee175f54d7a2afcb46b
raguiar2 <raguiar2@stanford.edu>
2017-11-10 21:18:13 -0800

    implemented first UCS

--- project/generate_poems.py
+++ project/generate_poems.py
@@ -3,19 +3,64 @@
 # write them to poems.txt
 import sys
 import random
-import nltk
-NUM_POEMS = 1
-import sys
+import string
+import util
 from util import PoemGenerator
+from ucs import GeneratePoemProblem
+import os 
+import wordsegUtil
+
+NUM_POEMS = 1
+POEMS_DIR = 'poet_parsing/poems'
+CORPUS = 'poet_parsing/corpus.txt'
 
 
-def generatePoem(firstline):
+
+
+
+# reads in one poem and updates the freq defaultdict
+def read_poem(file,words,printable):
+	lines = file.readlines()
+	for line in lines:
+		for word in line.split():
+			if word in words:
+				continue
+			# TODO: This could handle quotes/hyphens better
+			# word.translate strips punctuation
+			word = word.strip()
+			word = word.translate(None, string.punctuation)
+			# strips unicode characters out. This was causing some weird
+			# spacing in the generated poems. 
+			word = ''.join(list(filter(lambda x: x in printable, word)))
+			words.add(word)
+
+
+def read_corpus():
+	words = set()
+	for file in os.listdir(POEMS_DIR):
+		# skip hidden files
+		if file[0] == '.':
+			continue
+		filepath = POEMS_DIR+'/'+file
+		with open(filepath) as file:
+			#print('reading in file {}...'.format(filepath))
+			read_poem(file,words,string.printable)
+	return words
+
+
+
+
+def generate_poem(firstline):
     if len(firstline) == 0:
         return ''
+    firstline = firstline.split()
     ucs = util.UniformCostSearch(verbose=0)
-    ucs.solve(GeneratePoemProblem(firstline))
+    words = read_corpus()
+    print('training cost functions....')
+    unigramCost , bigramCost = wordsegUtil.makeLanguageModels(CORPUS)
+    print('cost functions trained!')
+    ucs.solve(GeneratePoemProblem(firstline,words, unigramCost,bigramCost)) #todo: learn cost function and add it as second param. 
     return ' '.join(ucs.actions)
-
 
 
 # only one argument for now. If the user specifies the -l flag
@@ -41,8 +86,10 @@ def main():
 	firstline = get_args()
 	poemgenerator = PoemGenerator("poems.txt")
 	poemgenerator.clear_baseline_file()
-	for _ in range(NUM_POEMS):
+	for i in range(NUM_POEMS):
 		poem = generate_poem(firstline)
+		print("Poem number {}:".format(i))
+		print(firstline + '\n' + poem)
 		poemgenerator.write_poem(poem)
 
 if __name__=='__main__':
